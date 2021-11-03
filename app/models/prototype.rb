786ddd994d9e712cf662f9883fe4ad48548dfe66
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-03 12:42:11 +0900

    バリデーションの設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,8 @@
 class Prototype < ApplicationRecord
-  belongs_to :user
+  validates :title,      presence: true
+  validates :catch_copy, presence: true
+  validates :concept,    presence: true
   has_one_attached :image
+
+  belongs_to :user
 end

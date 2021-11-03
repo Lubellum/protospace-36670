a7c5f38bb55d747264f37823a975ebf84801ed76
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-03 16:57:08 +0900

    トップページに投稿されたプロトタイプを表示

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,9 @@
 class Prototype < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  
+  validates :image,      presence: true
   validates :title,      presence: true
   validates :catch_copy, presence: true
   validates :concept,    presence: true
-  has_one_attached :image
-
-  belongs_to :user
 end

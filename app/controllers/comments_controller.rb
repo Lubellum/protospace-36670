cb5882767c3b5199fb633e053e4aaef9e8229829
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-06 21:19:06 +0900

    comment_paramsの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    
+    @comment = Comment.create(comment_params)
   end
 
   def show
     @comment = Comment.new
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end

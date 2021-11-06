8622348e7cc5844a32562e9512863396b9b866e2
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-06 21:02:52 +0900

    contentモデルのバリデーション設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many   :comments, dependent: :destroy
   
   validates :image,      presence: true
   validates :title,      presence: true

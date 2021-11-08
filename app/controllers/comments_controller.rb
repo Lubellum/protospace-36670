89f8e4842cf3243de55dc51f6175fccd787c2c7a
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-08 20:50:00 +0900

    コメント投稿機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype.id)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'prototypes#index'
   devise_for :users
-  resources :prototypes
-  resources :comments, only: [:create]
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
 end

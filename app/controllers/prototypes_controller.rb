3d71fa4afdf2afe1e1483e9c67853fce24dcf1e4
Lubellum <90450493+Lubellum@users.noreply.github.com>
2021-11-10 09:25:29 +0900

    move_to_indexの削除
    未ログイン時の処理は:authenticate_user!で良い

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :search]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -49,11 +50,5 @@ class PrototypesController < ApplicationController
 
   def set_tweet
     @prototype = Prototype.find(params[:id])
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 end
